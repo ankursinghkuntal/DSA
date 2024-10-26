308c92bc9a847abf76edeb81888fc574289449e8
ankursinghkuntal <ankursinghkuntal361@gmail.com>
2024-10-26 09:25:06 +0530

    going on

---
+++ POTD/Alternative Sorting.cpp
@@ -0,0 +1,19 @@
+    // vector<int> alternateSort(vector<int>& arr) {
+    //     int n = arr.size();
+    //     sort(arr.begin(),arr.end());
+    //     vector<int>brr;
+    //     int i = 0, j = n-1;
+    //     while(i <= j){
+    //         if(i<j){
+    //             brr.push_back(arr[j]);
+    //             brr.push_back(arr[i]);
+    //             i+=1;
+    //             j-=1;
+    //         }else{
+    //             brr.push_back(arr[i]);
+    //             i+=1;
+    //             j-=1;
+    //         }
+    //     }
+    //     return brr;
+    // }
