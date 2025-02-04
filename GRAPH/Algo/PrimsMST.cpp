950850c18d3f05b49d1b245617b65c2f3caf93d1
ankursinghkuntal <ankursinghkuntal361@gmail.com>
2025-02-04 18:30:16 +0530

    daily

--- GRAPH/Algo/PrimsMST.cpp
+++ GRAPH/Algo/PrimsMST.cpp
@@ -37,3 +37,43 @@
         
     //     return sum;
     // }
+
+
+
+
+
+
+ //     int spanningTree(int V, vector<vector<int>> adj[]) {
+    //     priority_queue< pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>>pq;
+    //     vector<pair<int,int>>mst;
+        
+    //     int sum = 0;
+        
+    //     vector<bool>vis(V,false);
+    //     pq.push({0,{0,-1}});
+        
+    //     while(!pq.empty()){
+            
+    //         int wt = pq.top().first;
+    //         int node = pq.top().second.first;
+    //         int parent = pq.top().second.second;
+    //         pq.pop();
+            
+    //         if(vis[node]) continue;
+            
+    //         if(parent != -1){
+    //             mst.push_back({parent,node});
+    //         }
+            
+    //         sum += wt;
+    //         vis[node] = true;
+            
+    //         for(auto adjNode : adj[node]){
+    //             int neighbour = adjNode[0];
+    //             int weight = adjNode[1];
+    //             pq.push({weight,{neighbour,node}});
+    //         }
+            
+    //     }
+    //     return sum;
+    // }
